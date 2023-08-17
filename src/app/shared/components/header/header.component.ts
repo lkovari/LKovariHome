610306b5caca85accfaf09f2e6b7fab8c366c1a1
LK <laszlo.kovary@gmail.com>
2023-08-17 23:40:49 +0200

    date fix

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,7 +6,7 @@ import * as angular from '@angular/forms';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  lastUpdateDate = new Date('08/17/2023 23:38 PM');
+  lastUpdateDate = new Date('08/17/2023 11:38 PM');
   lastUpdateTooltip = 'Numbers: last stage & share';
   angularVersion: any;
 
