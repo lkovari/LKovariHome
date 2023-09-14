9fbf223fc245f895391b061494130ae850f712cf
LK <laszlo.kovary@gmail.com>
2023-09-14 16:08:18 +0200

    show result when game completed

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,8 +6,8 @@ import * as angular from '@angular/forms';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  lastUpdateDate = new Date('09/10/2023 09:11 PM');
-  lastUpdateTooltip = 'fixed last page modal bug';
+  lastUpdateDate = new Date('09/14/2023 04:07 PM');
+  lastUpdateTooltip = 'show result when game completed';
   angularVersion: any;
 
   constructor() { }
