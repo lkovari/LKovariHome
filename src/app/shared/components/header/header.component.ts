c30e45fd4911eadb8cc1d241336efd5e206e92f1
LK <laszlo.kovary@gmail.com>
2024-03-26 21:42:48 +0100

    Update to v17.1.3 Update CV & pics

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,8 +6,8 @@ import * as angular from '@angular/forms';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  lastUpdateDate = new Date('03/19/2024 04:41 PM');
-  lastUpdateTooltip = 'select both wideo image whan wrap API. www.reddit.com';
+  lastUpdateDate = new Date('03/26/2024 09:38 PM');
+  lastUpdateTooltip = 'CV. & wrap API. www.reddit.com';
   angularVersion: any;
 
   constructor() { }
