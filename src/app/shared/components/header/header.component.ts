6fe1a1aca65f634bc0e53cab063e4ff1843ec1c9
lkovari <laszlo.kovary@gmail.com>
2024-12-01 15:46:58 +0100

    Replace LinkedIn profile url

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,8 +7,8 @@ import * as angular from '@angular/forms';
     standalone: false
 })
 export class HeaderComponent implements OnInit {
-  lastUpdateDate = new Date('11/26/2024 10:21 PM');
-  lastUpdateTooltip = 'Upgrade to Angular v19.0.1';
+  lastUpdateDate = new Date('12/01/2024 3:43 PM');
+  lastUpdateTooltip = 'Replace LinkedIn profile url';
   angularVersion!: string;
 
   constructor() { }
