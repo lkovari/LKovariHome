196365be8fff56edac0c286cc514231a7f924019
lkovari <laszlo.kovary@gmail.com>
2025-04-25 21:06:37 +0200

    update cv.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -15,8 +15,8 @@ import { ErrorNotificationService } from '../../services/error-handler/error-not
   imports: [MatToolbar, RouterLink, MatTooltip, DatePipe]
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  lastUpdateDate = new Date('04/14/2025 06:50 PM');
-  lastUpdateTooltip = 'Add vertical scroll to about page when it on mobile view';
+  lastUpdateDate = new Date('04/25/2025 09:07 PM');
+  lastUpdateTooltip = 'Updated my CV.';
   angularVersion!: string;
   showExclamationMark = false;
   errorEntries: ErrorEntry[] = [];
